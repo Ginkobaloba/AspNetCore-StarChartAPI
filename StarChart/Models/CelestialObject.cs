4d200a8a939b8eb4dbaf50e02b786b5a2d8ef230
Drew Mattick <drew.mattick@stsaviationgroup.com>
2019-04-09 16:29:51 -0500

    Completed the module

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using StarChart.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StarChart.Models
 {
     public class CelestialObject
     {
+
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        public int? OrbitedobjectId { get; set; }
+        [NotMapped]
+
+        public List<CelestialObject> Satellites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
+
     }
 }
