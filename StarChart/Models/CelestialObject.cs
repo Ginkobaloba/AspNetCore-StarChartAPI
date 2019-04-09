4f5d22c020c6783e7ead53791f8cf032bf9d9d79
Drew Mattick <drew.mattick@stsaviationgroup.com>
2019-04-09 16:30:54 -0500

    Completed the module

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -15,7 +15,7 @@ namespace StarChart.Models
 
         public string Name { get; set; }
 
-        public int? OrbitedobjectId { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
 
         public List<CelestialObject> Satellites { get; set; }
